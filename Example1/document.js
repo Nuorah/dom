0e558893a26c3f1a53d02dca86bce80deacb16cb
Haroun Moatassime <haroun.moatassime@gmail.com>
2019-03-20 12:35:31 +0100

    tp js dom example1

--- Example1/document.js
+++ Example1/document.js
@@ -3,6 +3,9 @@ var app = {
 		console.log('Document title: ', document.title);
 		document.title = 'Nouveau titre';
 		console.log('Document title: ', document.title);
+		console.log('Elements h1: ', document.getElementsByTagName('H1'));
+		console.log('Elements avec la classe text-red: ', document.getElementsByClassName('text-red'));
+		console.log("Element avec l'id title: ", document.getElementById('title'));
 	}
 };
 
